2722d5343ed9fc46fd8f3360bb58ea6f42bd43e8
Winssench <omar.chichaoui@gmail.com>
2020-11-03 11:42:40 +0100

    First

---
+++ instagram/Controllers/Core Tabs/CameraViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CameraViewController.swift
+//  instagram
+//
+//  Created by CHICHAOUI Omar on 03/11/2020.
+//  Copyright © 2020 HackingHouse. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
--- instagram/Controllers/Core Tabs/ExploreViewController.swift
+++ instagram/Controllers/Core Tabs/ExploreViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ExploreViewController.swift
 //  instagram
 //
 //  Created by CHICHAOUI Omar on 03/11/2020.
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ExploreViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+ 
 
 }
-
